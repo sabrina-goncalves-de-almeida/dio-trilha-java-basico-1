1b0abaaf54cc7e5b4e9422a4997d7f6f319f9252
sabrina-goncalves-de-almeida <sabrina.goncalves@dcx.ufpb.br>
2024-05-28 10:51:20 -0300

    new commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Digite seu nome:");
+        String nome = scan.nextLine();
+
+        System.out.println("Digite número da agência: ");
+        String numeroAgencia = scan.nextLine();
+
+        System.out.println("Digite número da conta: ");
+        int numeroConta = Integer.parseInt(scan.next());
+
+        System.out.println("Digite o saldo da conta: ");
+        double saldoConta = Double.parseDouble(scan.next());
+
+        System.out.println("Olá ".concat(nome +", obrigado por criar uma conta em nosso banco, sua agência é ").concat(numeroAgencia +", conta ").concat(numeroConta+" e seu saldo ").concat(saldoConta +" já está disponível para saque"));
+        
+        scan.close();
     }
+        
+    
 }
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # dio-trilha-java-basico
+
+## Desafio de código java básico
+Criar uma classe java `BancoTerminal.java`, localizada no projeto `ContaBanco`, que recebe os dados via terminal, com tipos diferentes de variaveis e imprima todos juntos com uso de `concat()`.
+
+## Exemplo ofertado pelo instrutor
+
+### Entradas e varáveis
+| Atributo  | Tipo     | Exemplo   
+| --------- | ---------| ------- 
+| Numero    | Inteiro  | 1021 
+| Agencia   | Texto    | 067-8
+| Nome Cliente | Texto    | MARIO ANDRADE
+| Saldo | Decimal |237.48
+
+### Reposta esperada
+
+```
+Olá MARIO ANDRADE, obrigado por criar uma conta em nosso banco, sua agência é 067-8, conta 1021 e seu saldo 237.48 já está disponível para saque
+```
+
+
+## Referencias
+
+- [DIO](https://www.dio.me)
+- [Gleyson Sampaio](https://github.com/glysns)
+
+
